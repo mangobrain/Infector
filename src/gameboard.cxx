0edc76986db0c9810b55d138710a23c2c27c8c4f
Philip Allison <sane@not.co.uk>
2008-10-10 16:42:27 +0100

    Capitalise I on project name
    Signed-off-by: Philip Allison <sane@not.co.uk>

--- src/gameboard.cxx
+++ src/gameboard.cxx
@@ -1,19 +1,19 @@
 // Copyright 2008 Philip Allison <sane@not.co.uk>
 
-//    This file is part of infector.
+//    This file is part of Infector.
 //
-//    infector is free software: you can redistribute it and/or modify
+//    Infector is free software: you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
 //    the Free Software Foundation, either version 3 of the License, or
 //    (at your option) any later version.
 //
-//    infector is distributed in the hope that it will be useful,
+//    Infector is distributed in the hope that it will be useful,
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of
 //    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //    GNU General Public License for more details.
 //
 //    You should have received a copy of the GNU General Public License
-//    along with infector.  If not, see <http://www.gnu.org/licenses/>.
+//    along with Infector.  If not, see <http://www.gnu.org/licenses/>.
 
 
 //
